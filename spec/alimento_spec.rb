9fc43d77fd4bc8cfc93383749fe665451dd207d9
beita <bei.g.g.97@gmail.com>
2018-11-09 04:10:42 +0000

    Error de funcion no existe la función

--- spec/alimento_spec.rb
+++ spec/alimento_spec.rb
@@ -3,6 +3,11 @@ require "./spec/spec_helper"
 
 RSpec.describe Alimento do
     before :each do
-        @a=Alimento.new(12,34,45,56,12,22,12)
+        @a=Alimento.new(12,34,45,56,12,22,12,0,0)
+        @ap=Alimento.new(12,34,45,56,12,22,12,8,200)
     end 
+    it "Valor energetico correcto" do
+        expect(@a.valor).to eq(2584)
+        expect(@ap.valorg).to_eq(90)
+    end
 end 
