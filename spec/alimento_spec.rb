02f8865f1b008c4e430f903fbaae5597dc68937e
beita <bei.g.g.97@gmail.com>
2018-11-09 04:17:22 +0000

    uncion creada y correcto

--- spec/alimento_spec.rb
+++ spec/alimento_spec.rb
@@ -3,12 +3,12 @@ require "./spec/spec_helper"
 
 RSpec.describe Alimento do
     before :each do
-        @a=Alimento.new(12,34,45,56,12,22,12,0,0)
-        @ap=Alimento.new(12,34,45,56,12,22,12,8,200)
+        @a=Alimento.new(12.0,34.0,45.0,56.0,12.0,22.0,12.0,0,0)
+        @ap=Alimento.new(10.0,30.0,40.0,50.0,10.0,20.0,10.0,8.0,200.0)
     end 
     it "Valor energetico correcto" do
         expect(@a.valor).to eq(2584)
-        expect(@ap.valorg).to eq(618)
-        expect(@a.ir).to eq(8)
+        expect(@ap.valorg).to eq(550.0)
+        expect(@a.ir).to eq(25.84)
     end
 end 
