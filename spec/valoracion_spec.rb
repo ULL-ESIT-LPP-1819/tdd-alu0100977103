87128a171ea550121aa40a65f9fbc8b14ce56fd6
beita <bei.g.g.97@gmail.com>
2018-11-23 01:28:01 +0000

    Generada la gema Guard

--- spec/valoracion_spec.rb
+++ spec/valoracion_spec.rb
@@ -4,6 +4,9 @@ RSpec.describe Valoracion do
     before :each do
         @ind_1=Valoracion.new(12.0,34.0,45.0,56.0,12.0,22.0,12.0,0,0,"Juan",0,0.038)
         @ind_2=Valoracion.new(10.0,30.0,40.0,50.0,10.0,20.0,10.0,8.0,200.0,"Maria",1,0.048)
+        @ind_3=Valoracion.new(50.0,30.0,40.0,50.0,10.0,20.0,10.0,8.0,30.0,"Lucia",1,0.034)
+        @ind_4=Valoracion.new(20.0,15.0,40.0,50.0,10.0,20.0,10.0,8.0,45.0,"Luis",1,0.040)
+        @ind_5=Valoracion.new(43.0,17.0,32.0,50.0,10.0,20.0,10.0,8.0,60.0,"Daniel",1,0.042)
     end 
     
     it "Ver si es o no obeso el individuo" do
