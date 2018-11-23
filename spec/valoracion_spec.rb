2d95655daed04d6cffad1a36de373df8243ca096
beita <bei.g.g.97@gmail.com>
2018-11-23 01:02:24 +0000

    Error no existe el fichero valoracion

--- spec/valoracion_spec.rb
+++ spec/valoracion_spec.rb
@@ -3,6 +3,6 @@ require "./spec/spec_helper"
 RSpec.describe Valoracion do
     before :each do
         @ind_1=Valoracion.new(12.0,34.0,45.0,56.0,12.0,22.0,12.0,0,0,"Juan",0)
-        @ind_2=Valoracion.new(10.0,30.0,40.0,50.0,10.0,20.0,10.0,8.0,200.0,"Soledad",1)
+        @ind_2=Valoracion.new(10.0,30.0,40.0,50.0,10.0,20.0,10.0,8.0,200.0,"Maria",1)
     end 
 end
