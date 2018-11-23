b58e8fe7164e033b370a0732cf92e6b5b13723e1
beita <bei.g.g.97@gmail.com>
2018-11-23 01:14:59 +0000

    Funcion que ve si el inidividuo tiene o no obesidad implementado

--- lib/gema/valoracion.rb
+++ lib/gema/valoracion.rb
@@ -1,11 +1,21 @@
 class Valoracion<Alimento
    
-    def initialize(valor,grasas,saturadas,hidratos,azucares,proteinas,sal,porciones,peso,nombre,v_consult)
+    def initialize(valor,grasas,saturadas,hidratos,azucares,proteinas,sal,porciones,peso,nombre,v_consult,talla)
         super(valor,grasas,saturadas,hidratos,azucares,proteinas,sal,porciones,peso)
         @nombre=nombre
         @v_consult=v_consult
+        @talla = talla
     end
     
-    
+    def IMC
+        @talla_t=@talla*@talla
+        @obesidad=@peso/@talla_t
+        @tratamiento=0
+        if @obesidad >=30
+            @tratamiento=1
+        else
+            @tratamiento=0
+        end
+    end
 
 end
--- spec/valoracion_spec.rb
+++ spec/valoracion_spec.rb
@@ -2,12 +2,13 @@ require "./spec/spec_helper"
 
 RSpec.describe Valoracion do
     before :each do
-        @ind_1=Valoracion.new(12.0,34.0,45.0,56.0,12.0,22.0,12.0,0,0,"Juan",0)
-        @ind_2=Valoracion.new(10.0,30.0,40.0,50.0,10.0,20.0,10.0,8.0,200.0,"Maria",1)
+        @ind_1=Valoracion.new(12.0,34.0,45.0,56.0,12.0,22.0,12.0,0,0,"Juan",0,0.038)
+        @ind_2=Valoracion.new(10.0,30.0,40.0,50.0,10.0,20.0,10.0,8.0,200.0,"Maria",1,0.048)
     end 
     
     it "Ver si es o no obeso el individuo" do
-        expect(@ind_1.IMC).to eq(1)
+        expect(@ind_1.IMC).to eq(0)  #No tiene obesidad
+        expect(@ind_2.IMC).to eq(1)  #Tiene obesidad
     end
     
 end
