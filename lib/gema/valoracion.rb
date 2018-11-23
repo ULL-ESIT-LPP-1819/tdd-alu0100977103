6fbe29ca0bea2f8f395fd42e30f53bcb5ecb5a27
beita <bei.g.g.97@gmail.com>
2018-11-23 01:08:27 +0000

    Funcion de obesidad no implementada, por tanto no funciona

--- lib/gema/valoracion.rb
+++ lib/gema/valoracion.rb
@@ -1,7 +1,11 @@
 class Valoracion<Alimento
+   
     def initialize(valor,grasas,saturadas,hidratos,azucares,proteinas,sal,porciones,peso,nombre,v_consult)
         super(valor,grasas,saturadas,hidratos,azucares,proteinas,sal,porciones,peso)
         @nombre=nombre
         @v_consult=v_consult
     end
+    
+    
+
 end
--- spec/valoracion_spec.rb
+++ spec/valoracion_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe Valoracion do
         @ind_1=Valoracion.new(12.0,34.0,45.0,56.0,12.0,22.0,12.0,0,0,"Juan",0)
         @ind_2=Valoracion.new(10.0,30.0,40.0,50.0,10.0,20.0,10.0,8.0,200.0,"Maria",1)
     end 
+    
+    it "Ver si es o no obeso el individuo" do
+        expect(@ind_1.IMC).to eq(1)
+    end
+    
 end
