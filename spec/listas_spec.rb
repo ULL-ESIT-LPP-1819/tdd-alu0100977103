dabbeb983a3e39c42b62ca6a444fa86fbe322ef9
beita <bei.g.g.97@gmail.com>
2018-12-06 16:48:23 +0000

    Enumerable fallando no existe modulo enumerable

--- spec/listas_spec.rb
+++ spec/listas_spec.rb
@@ -38,6 +38,10 @@ RSpec.describe LinkedList do
          end 
     end
     
+    it "Enumerable" do
+        expect(@lista.max).to_eq(@et_1)
+    end
+    
      
      #sudo -u postges psql
      
