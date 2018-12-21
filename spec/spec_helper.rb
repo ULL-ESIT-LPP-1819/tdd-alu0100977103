bd45e9135d620d2a0ddbc538442b2714ef5891a4
beita <bei.g.g.97@gmail.com>
2018-12-21 01:14:18 +0000

    Proabdno coverrals

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ require "gema"
 require "coveralls"
 Coveralls.wear!
 
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
